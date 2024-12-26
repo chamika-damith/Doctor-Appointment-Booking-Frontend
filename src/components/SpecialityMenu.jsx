2ddd95bf39991be3ed6c929826e1d44e615672fa
chamika-damith <chamikadamith9@gmail.com>
2024-12-26 23:37:24 +0530

    sepecialityMenu section completed

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 import { specialityData } from "../assets/assets"
 const SpecialityMenu = () => {
   return (
-    <div id="speciality">
-        <h1>Find by Speciality</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ullam perferendis ex sit, a illo!</p>
-        <div>
+    <div id="speciality" className="flex flex-col items-center gap-4 py-16 text-gray-800">
+        <h1 className="text-3xl font-medium">Find by Speciality</h1>
+        <p className="sm:w-1/3 text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ullam perferendis ex sit, a illo!</p>
+        <div className="flex sm:justify-center gap-4 w-full overflow-scroll">
             {specialityData.map((item,index)=>(
-                <Link key={index} to={`/doctors/${item.speciality}`}>
-                    <img src={item.image} alt=""/>
+                <Link onClick={()=>scrollTo(0,0)} className="flex flex-col mt-3 items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} to={`/doctors/${item.speciality}`}>
+                    <img className="w-12 sm:w-24 mb-2" src={item.image} alt=""/>
                     <p>{item.speciality}</p>
                 </Link>
             ))}
