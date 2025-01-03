d55fa11aa210f812d12f922266b412cfd8cd4fcd
chamika-damith <chamikadamith9@gmail.com>
2025-01-03 13:16:11 +0530

    complete banner section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
@@ -7,6 +8,7 @@ const Home = () => {
       <Header />
       <SpecialityMenu />
       <TopDoctors />
+      <Banner />
     </div>
   )
 }
